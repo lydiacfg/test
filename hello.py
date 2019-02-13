179bc9d2215ea16f1d8be746eecbcae423b83d67
Lydia Carr <lonlc30@LONLC30-M2.local>
2019-02-13 10:26:40 +0000

    first file

---
+++ hello.py
@@ -0,0 +1,26 @@
+print("Hello, World!")
+
+print(5-6)
+print(8*9)
+print(6/2)
+print(5//2)
+print(5.0/2)
+print(5%2)
+print(2*(10+3))
+print(2**4)
+
+print("Bob" * 3)
+print("Bob" + 3)
+print("hello".upper())
+print("GOODBYE".lower())
+print("the lord of the rings".title())
+
+age ​=​ ​5
+like ​=​ ​"painting"
+a_longer_name ​=​ ​"hello, CFG!"
+like ​=​ ​"painting"
+age_description ​=​ ​"My age is {} and I like {}."​.format(age, like)
+
+print(age)
+print(a_longer_name)
+print(age_description)
